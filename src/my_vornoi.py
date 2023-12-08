7e6a1bba2a818aa8194cfd6db703548605006bb5
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2023-12-08 18:07:24 -0500

    why was this stuff here

--- src/my_vornoi.py
+++ src/my_vornoi.py
@@ -1,29 +1,6 @@
 import numpy as np
-from scipy._lib.decorator import decorator as _decorator
 
-__all__ = ['delaunay_plot_2d', 'convex_hull_plot_2d', 'voronoi_plot_2d']
-
-
-@_decorator
-def _held_figure(func, obj, ax=None, **kw):
-    import matplotlib.pyplot as plt
-
-    if ax is None:
-        fig = plt.figure()
-        ax = fig.gca()
-        return func(obj, ax=ax, **kw)
-
-    # As of matplotlib 2.0, the "hold" mechanism is deprecated.
-    # When matplotlib 1.x is no longer supported, this check can be removed.
-    was_held = getattr(ax, 'ishold', lambda: True)()
-    if was_held:
-        return func(obj, ax=ax, **kw)
-    try:
-        ax.hold(True)
-        return func(obj, ax=ax, **kw)
-    finally:
-        ax.hold(was_held)
-
+__all__ = ['voronoi_plot_2d']
 
 def _adjust_bounds(ax, points):
     margin = 0.1 * points.ptp(axis=0)
@@ -33,124 +10,6 @@ def _adjust_bounds(ax, points):
     ax.set_ylim(xy_min[1], xy_max[1])
 
 
-@_held_figure
-def delaunay_plot_2d(tri, ax=None):
-    """
-    Plot the given Delaunay triangulation in 2-D
-
-    Parameters
-    ----------
-    tri : scipy.spatial.Delaunay instance
-        Triangulation to plot
-    ax : matplotlib.axes.Axes instance, optional
-        Axes to plot on
-
-    Returns
-    -------
-    fig : matplotlib.figure.Figure instance
-        Figure for the plot
-
-    See Also
-    --------
-    Delaunay
-    matplotlib.pyplot.triplot
-
-    Notes
-    -----
-    Requires Matplotlib.
-
-    Examples
-    --------
-
-    >>> import numpy as np
-    >>> import matplotlib.pyplot as plt
-    >>> from scipy.spatial import Delaunay, delaunay_plot_2d
-
-    The Delaunay triangulation of a set of random points:
-
-    >>> rng = np.random.default_rng()
-    >>> points = rng.random((30, 2))
-    >>> tri = Delaunay(points)
-
-    Plot it:
-
-    >>> _ = delaunay_plot_2d(tri)
-    >>> plt.show()
-
-    """
-    if tri.points.shape[1] != 2:
-        raise ValueError("Delaunay triangulation is not 2-D")
-
-    x, y = tri.points.T
-    ax.plot(x, y, 'o')
-    ax.triplot(x, y, tri.simplices.copy())
-
-    _adjust_bounds(ax, tri.points)
-
-    return ax.figure
-
-
-@_held_figure
-def convex_hull_plot_2d(hull, ax=None):
-    """
-    Plot the given convex hull diagram in 2-D
-
-    Parameters
-    ----------
-    hull : scipy.spatial.ConvexHull instance
-        Convex hull to plot
-    ax : matplotlib.axes.Axes instance, optional
-        Axes to plot on
-
-    Returns
-    -------
-    fig : matplotlib.figure.Figure instance
-        Figure for the plot
-
-    See Also
-    --------
-    ConvexHull
-
-    Notes
-    -----
-    Requires Matplotlib.
-
-
-    Examples
-    --------
-
-    >>> import numpy as np
-    >>> import matplotlib.pyplot as plt
-    >>> from scipy.spatial import ConvexHull, convex_hull_plot_2d
-
-    The convex hull of a random set of points:
-
-    >>> rng = np.random.default_rng()
-    >>> points = rng.random((30, 2))
-    >>> hull = ConvexHull(points)
-
-    Plot it:
-
-    >>> _ = convex_hull_plot_2d(hull)
-    >>> plt.show()
-
-    """
-    from matplotlib.collections import LineCollection
-
-    if hull.points.shape[1] != 2:
-        raise ValueError("Convex hull is not 2-D")
-
-    ax.plot(hull.points[:, 0], hull.points[:, 1], 'o')
-    line_segments = [hull.points[simplex] for simplex in hull.simplices]
-    ax.add_collection(LineCollection(line_segments,
-                                     colors='k',
-                                     linestyle='solid'))
-    _adjust_bounds(ax, hull.points)
-
-    return ax.figure
-
-
-@_held_figure
 def voronoi_plot_2d(vor, ax=None, **kw):
     """
     Plot the given Voronoi diagram in 2-D
@@ -178,41 +37,6 @@ def voronoi_plot_2d(vor, ax=None, **kw):
     -------
     fig : matplotlib.figure.Figure instance
         Figure for the plot
-
-    See Also
-    --------
-    Voronoi
-
-    Notes
-    -----
-    Requires Matplotlib.
-
-    Examples
-    --------
-    >>> import numpy as np
-    >>> import matplotlib.pyplot as plt
-    >>> from scipy.spatial import Voronoi, voronoi_plot_2d
-
-    Create a set of points for the example:
-
-    >>> rng = np.random.default_rng()
-    >>> points = rng.random((10,2))
-
-    Generate the Voronoi diagram for the points:
-
-    >>> vor = Voronoi(points)
-
-    Use `voronoi_plot_2d` to plot the diagram:
-
-    >>> fig = voronoi_plot_2d(vor)
-
-    Use `voronoi_plot_2d` to plot the diagram again, with some settings
-    customized:
-
-    >>> fig = voronoi_plot_2d(vor, show_vertices=False, line_colors='orange',
-    ...                       line_width=2, line_alpha=0.6, point_size=2)
-    >>> plt.show()
-
     """
     from matplotlib.collections import LineCollection
 
